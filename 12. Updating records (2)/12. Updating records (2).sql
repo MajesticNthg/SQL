7b52a9f6e6cc1c0c7536a81e366ddec20dc58407
Danil <147924429+MajesticNthg@users.noreply.github.com>
2025-05-22 16:50:48 +1000

    Create project 12. Updating records (2)

---
+++ 12. Updating records (2)/12. Updating records (2).sql
@@ -0,0 +1,12 @@
+Update [Order Details]
+Set Discount = 0.20
+Where Quantity > 50;
+
+Contacts
+Set City = 'Piter', Country = 'Russia'
+Where City = 'Berlin' AND Country = 'Germany';
+
+Insert Into Shippers (CompanyName, Phone)
+Values ('Rus Express', '(503) 555-7777'), ('International Company', '(503) 555-3333');
+Delete From Shippers
+Where ShipperID > 3;
